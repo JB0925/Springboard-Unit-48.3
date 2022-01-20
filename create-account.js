c57a2481ba725fe23955f3417f828e6a45b4d43d
Jesse Brink <jbrink0925@gmail.com>
2022-01-19 19:47:29 -0500

    complete assignment for Springboard.

--- create-account.js
+++ create-account.js
@@ -1,5 +1,33 @@
 function createAccount(pin, amount) {
+  let accountPin = pin;
+  let accountAmount = amount || 0;
+  return {
+    checkBalance(userPin) {
+      if (userPin !== accountPin) return "Invalid PIN.";
+      return `$${accountAmount}`;
+    },
 
+    deposit(userPin, amountToAdd) {
+      if (userPin !== accountPin) return "Invalid PIN.";
+      accountAmount += amountToAdd;
+      return `Succesfully deposited $${amountToAdd}. Current balance: $${accountAmount}.`;
+    },
+
+    withdraw(userPin, amountToWithdraw) {
+      if (userPin !== accountPin) return "Invalid PIN.";
+      if (accountAmount - amountToWithdraw < 0) {
+        return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      }
+      accountAmount -= amountToWithdraw;
+      return `Succesfully withdrew $${amountToWithdraw}. Current balance: $${accountAmount}.`;
+    },
+
+    changePin(userPin, newUserPin) {
+      if (userPin !== accountPin) return "Invalid PIN.";
+      accountPin = newUserPin;
+      return "PIN successfully changed!";
+    },
+  };
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,20 @@
 function guessingGame() {
+  let hasWon = false;
+  let winningNumber = Math.floor(Math.random() * 100);
+  let numberOfGuesses = 0;
+  return (userGuess) => {
+    numberOfGuesses++;
+    if (hasWon) return "The game is over, you already won!";
 
+    if (userGuess === winningNumber) {
+      hasWon = true;
+      return `You win! You found ${winningNumber} in ${numberOfGuesses} guesses.`;
+    } else if (userGuess < winningNumber) {
+      return `${userGuess} is too low!`;
+    } else {
+      return `${userGuess} is too high!`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
